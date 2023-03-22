ad7f673d7ebd0d2e1c672aaecd5bad6fa64dce31
Shyamal Kirpan <shyamalkirpan20@gmail.com>
2023-03-22 21:40:54 +0530

    style: :sparkles:

--- ToDoLists.jsx
+++ ToDoLists.jsx
@@ -4,7 +4,7 @@ const ToDoLists = (props) => {
   return (
     <>
       <div className="todo-style">
-        <i class="fa fa-times" aria-hidden="true" />
+        <i class="fa-sharp fa-solid fa-circle-xmark"></i>
         <li>{props.text}</li>
       </div>
     </>
